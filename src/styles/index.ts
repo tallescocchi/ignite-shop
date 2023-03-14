d31d1ca44d13ed54fe1711ac91a3a5862b1b902e
Talles Cocchi <78833791+tallescocchi@users.noreply.github.com>
2023-03-14 20:32:00 -0300

    First Commit

---
+++ src/styles/index.ts
@@ -0,0 +1,33 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+  config,
+  theme,
+  createTheme,
+  css,
+  styled,
+  globalCss,
+  keyframes,
+  getCssText,
+} = createStitches({
+  theme: {
+    colors: {
+      white: "#FFF",
+
+      gray900: '#121214',
+      gray800: '#202024',
+      gray300: '#C4C4CC',
+      gray100: '#E1E1E6',
+
+      green500: '#00875f',
+      green300: '#00b37e',
+    },
+
+    fontSizes: {
+      md: '1.125rem',
+      lg: '1.25rem',
+      xl: '1.5rem',
+      '2xl': '2rem'
+    }
+  }
+})
